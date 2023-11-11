fb3add11601c72db24465ad1fc4c4f3ba787c015
Roman Kozulia <romanmuni8@gmail.com>
2023-11-11 18:04:12 -0500

    Initial Commit

---
+++ IntegralEyesight/IntegralEyesightApp.swift
@@ -0,0 +1,17 @@
+//
+//  IntegralEyesightApp.swift
+//  IntegralEyesight
+//
+//  Created by Roman Kozulia on 11/11/23.
+//
+
+import SwiftUI
+
+@main
+struct IntegralEyesightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
